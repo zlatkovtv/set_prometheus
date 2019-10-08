b8865398d4486cf1b038b26fa5e8f5ad34fd5b58
unknown <zack.obeid@gmail.com>
2019-10-07 18:09:40 -0700

    Changed NearLiteral to handle phrases and wildcard queries

--- src/cecs429/query/NearLiteral.java
+++ src/cecs429/query/NearLiteral.java
@@ -5,6 +5,7 @@ import cecs429.index.Posting;
 import cecs429.text.TokenProcessor;
 import cecs429.util.MergeOperations;
 
+import javax.management.Query;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,10 +26,25 @@ public class NearLiteral implements QueryComponent {
         //splitting "near/#" string to get integer value
         String[] nearParts = near.split("\\/");
         int integerK = Integer.valueOf(nearParts[1]);
-        String first = tokenProcessor.processQueryToken(parts.get(0));
-        String second = tokenProcessor.processQueryToken(parts.get(2));
+        String first = parts.get(0);
+        String second = parts.get(2);
 
-        List<Posting> result = MergeOperations.postionalIntersect(index.getPostings(first), index.getPostings(second), integerK);
-        return MergeOperations.normalizeToUnique(result);
+        QueryComponent firstComponent = getType(first);
+        QueryComponent secondComponent = getType(second);
+
+        return MergeOperations.postionalIntersect(firstComponent.getPostings(index, tokenProcessor), secondComponent.getPostings(index,tokenProcessor), integerK);
+
+    }
+
+    private QueryComponent getType(String string) {
+        if(string.charAt(0) == '\"' && string.charAt(string.length()-1) == '\"') {
+            //phrase literal
+            return new PhraseLiteral(string.substring(1, string.length()-2));
+        } else if(string.contains("*")) {
+            //wildcard literal
+            return new WildcardLiteral(string);
+        } else {
+            return new TermLiteral(string);
+        }
     }
 }
